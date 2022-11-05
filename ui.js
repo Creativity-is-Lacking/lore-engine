4bb1f825358b58844d2b3003150d4c1aff1612b2
Creativity-is-Lacking <55517413+Creativity-is-Lacking@users.noreply.github.com>
2022-11-05 14:12:44 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,6 +1,6 @@
 var button = document.getElementById("add");
 button.onclick = function(){
-  boxes = boxes.slice(1,2);
+  boxes = boxes.slice(1,3);
   boxes.unshift();
-  elems = 1;
+  elems = 2;
 };
