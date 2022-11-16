451b22bb300470568bb66906fdf2d1cf4ecabe1d
Creativity-is-Lacking <55517413+Creativity-is-Lacking@users.noreply.github.com>
2022-11-16 09:12:44 -0500

    Update drag.js

--- drag.js
+++ drag.js
@@ -100,8 +100,8 @@ function linkLine(id, box1, box2, color){
     c.strokeStyle = '#ffffff';
     c.beginPath();
     oc.beginPath();
-    c.moveTo(box1.x,box1.y);
-    oc.moveTo(box1.x,box1.y);
+    c.moveTo(box1.x + box1.w,box1.y + (box1.h/2));
+    oc.moveTo(box1.x + box1.w,box1.y + (box1.h/2));
     c.bezierCurveTo(box1.x + box1.w,box1.y + (box1.h/2),231,207,box2.x,box2.y + (box2.h/2));
     oc.bezierCurveTo(box1.x + box1.w,box1.y + (box1.h/2),231,207,box2.x,box2.y + (box2.h/2));
     c.stroke();
