41ec1b78506b3b3dc337aad0db24a04ccdcfb400
Creativity-is-Lacking <55517413+Creativity-is-Lacking@users.noreply.github.com>
2022-11-22 08:36:27 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -3,8 +3,32 @@ var link = document.getElementById("link");
 var justify = document.getElementById("justify");
 var order = document.getElementById("order");
 
-move.onclick = function() {mode=''}
-link.onclick = function() {mode='link'}
-justify.onclick = function() {mode='justify'}
-order.onclick = function() {mode='order'} //will modify with modal to determine what direction to order
+move.onclick = function() {
+  mode='';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  move.className = 'active';
+}
+link.onclick = function() {
+  mode='link';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  link.className = 'active';
+}
+justify.onclick = function() {
+  mode='justify';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  justify.className = 'active';
+}
+order.onclick = function() {
+  mode='order';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  order.className = 'active'
+} //will modify with modal to determine what direction to order
 //to add: offset(?), edit ...
