f37cfb2dfe14c3a631bb0d748c5a63f5bdd13c6b
Creativity-is-Lacking <55517413+Creativity-is-Lacking@users.noreply.github.com>
2022-12-07 16:57:15 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,7 +1,13 @@
 var move = document.getElementById("move");
+var edit = document.getElementById("edit");
 var link = document.getElementById("link");
-var justify = document.getElementById("justify");
-var order = document.getElementById("order");
+var lock = document.getElementById("lock");
+var justify = document.getElementById("Pjustify");
+var Halign = document.getElementById("Halign");
+var Valign = document.getElementById("Valign");
+var order = document.getElementById("Porder");
+var forward = document.getElementById("Forder");
+var backward = document.getElementById("Border");
 
 move.onclick = function() {
   mode='';
@@ -10,6 +16,14 @@ move.onclick = function() {
     document.getElementsByClassName("active")[i].className = '';
   move.className = 'active';
 }
+edit.onclick = function() {
+  mode='edit';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  edit.className = 'active';
+  //show edit modal
+}
 link.onclick = function() {
   mode='link';
   let len = document.getElementsByClassName("active").length;
@@ -17,12 +31,35 @@ link.onclick = function() {
     document.getElementsByClassName("active")[i].className = '';
   link.className = 'active';
 }
+lock.onclick = function() {
+  mode='lock';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  lock.className = 'active';
+  //get confirmation before locking
+}
 justify.onclick = function() {
   mode='justify';
   let len = document.getElementsByClassName("active").length;
   for(var i = 0; i < len; i++)
     document.getElementsByClassName("active")[i].className = '';
   justify.className = 'active';
+  //show modal asking how to justify
+}
+Halign.onclick = function() {
+  mode='Halign';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  Halign.className = 'active';
+}
+Valign.onclick = function() {
+  mode='Valign';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  Valign.className = 'active';
 }
 order.onclick = function() {
   mode='order';
@@ -30,5 +67,19 @@ order.onclick = function() {
   for(var i = 0; i < len; i++)
     document.getElementsByClassName("active")[i].className = '';
   order.className = 'active'
-} //will modify with modal to determine what direction to order
-//to add: offset(?), edit ...
+  //show modal asking how to order
+} 
+forward.onclick = function() {
+  mode='Forder';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  forward.className = 'active';
+}
+backward.onclick = function() {
+  mode='Border';
+  let len = document.getElementsByClassName("active").length;
+  for(var i = 0; i < len; i++)
+    document.getElementsByClassName("active")[i].className = '';
+  backward.className = 'active';
+}
